93d3d6685651caafa75cc43b1cbd9d1d15495689
José Manuel Cortes Cerón <230110688@itsoeh.edu.mx>
2025-03-13 18:47:37 -0600

    Add get_all_items and get_size methods to Stack class

--- src/stack_hanoi.py
+++ src/stack_hanoi.py
@@ -3,7 +3,7 @@ class Stack:
         """Initialize a new stack with the given name."""
         self.name = name
         self.items = []
-        
+
     def push(self, item):
         """Add an item to the top of the stack."""
         self.items.append(item)
@@ -13,3 +13,10 @@ class Stack:
         if self.items:
             return self.items.pop()
         raise IndexError("Pop from empty stack")
+    def get_all_items(self):
+        """Return all items in the stack (base first, top last)."""
+        return self.items
+
+    def get_size(self):
+        """Return the number of items in the stack."""
+        return len(self.items)
