048fcc042cbfb8b56f4669ba7ba485a01c92d8a0
José Manuel Cortes Cerón <230110688@itsoeh.edu.mx>
2025-03-13 18:46:06 -0600

    Add push and pop methods to Stack class

--- src/stack_hanoi.py
+++ src/stack_hanoi.py
@@ -3,3 +3,13 @@ class Stack:
         """Initialize a new stack with the given name."""
         self.name = name
         self.items = []
+        
+    def push(self, item):
+        """Add an item to the top of the stack."""
+        self.items.append(item)
+
+    def pop(self):
+        """Remove and return the top item of the stack."""
+        if self.items:
+            return self.items.pop()
+        raise IndexError("Pop from empty stack")
